90d31514daa8eb436d4b2404064d4725a2b600c4
s3abiscuit <syjefb2@outlook.com>
2020-01-01 20:36:21 +0800

    Update HashMap.java

--- HashMap.java
+++ HashMap.java
@@ -1,5 +1,16 @@
 public class HashMap<K,V> extends AbstractMap<K,V>
     implements Map<K,V>, Cloneable, Serializable {
+    // 关键变量
+    // capacity: (1. Default 16, 2. >=initialCapacity, 3. >= map.size())
+    // loadFactor, threshold
+    // table, keySet, entrySet, Values
+    
+    // 常量
+    // 构造方法
+    // 遍历 EntrySet, EntrySetIterator, KeySet, KeySetIterator, Values, ValueIterator
+    // 增删改查 putVal(), removeNode(), getNode(), replace()也是getNode
+    
+    // 位操作, clone, TreeNode
 
     private static final long serialVersionUID = 362498820763181265L;
     
