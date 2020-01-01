c50a8113f54d4ab0d2742eeb32fe7319845e9a64
s3abiscuit <syjefb2@outlook.com>
2020-01-01 20:58:51 +0800

    Update HashMap.java

--- HashMap.java
+++ HashMap.java
@@ -12,6 +12,10 @@ public class HashMap<K,V> extends AbstractMap<K,V>
     // 辅助方法 resize(), hash(), tableForSize()
     
     // 位操作, clone, TreeNode
+    // 参考
+    // https://www.cnblogs.com/xiaoxi/p/7233201.html
+    // https://blog.csdn.net/zxt0601/article/details/77413921
+    // https://segmentfault.com/a/1190000012926722
 
     private static final long serialVersionUID = 362498820763181265L;
     
