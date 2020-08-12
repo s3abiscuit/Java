59f05c55d806f158ece1b8d9c4d397786a86b0d7
s3abiscuit <syjefb2@outlook.com>
2020-08-12 14:00:47 +0800

    ProducerAndConsumer

--- src/concurrent/ProducerAndConsumer.java
+++ src/concurrent/ProducerAndConsumer.java
@@ -2,10 +2,14 @@ package concurrent;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class ProducerAndConsumer {
     public static void main(String[] args) {
-        Factory factory = new Factory();
+        // Factory factory = new Factory();  // use wait notify
+        Factory1 factory = new Factory1();   // use lock condition
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -56,6 +60,47 @@ class Factory {
                     }
                 }
             }
+        }
+    }
+}
+
+class Factory1 {
+    private List<String> list = new ArrayList<>();
+    Lock lock = new ReentrantLock();
+    Condition condition = lock.newCondition();
+    public void produce(String str){
+        while(true){
+            lock.lock();
+            if(list.size()==0){
+                System.out.println("producer");
+                list.add(str);
+                condition.signal();
+            }else{
+                try {
+                    condition.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            lock.unlock();
+        }
+    }
+
+    public void consume(){
+        while(true){
+            lock.lock();
+            if(list.size()!=0){
+                System.out.println("consumer");
+                list.remove(0);
+                condition.signal();
+            }else{
+                try {
+                    condition.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            lock.unlock();
         }
     }
 }
