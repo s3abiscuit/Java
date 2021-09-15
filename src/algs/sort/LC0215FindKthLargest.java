0c791a36074323fc4da092108119bd8d736f375c
a11 <syjefb2@outlook.com>
2021-09-15 20:40:10 +0800

    Add algs.sort.LC0215FindKthLargest

---
+++ src/algs/sort/LC0215FindKthLargest.java
@@ -0,0 +1,91 @@
+//给定整数数组 nums 和整数 k，请返回数组中第 k 个最大的元素。
+//
+// 请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+//
+//
+//
+// 示例 1:
+//
+//
+//输入: [3,2,1,5,6,4] 和 k = 2
+//输出: 5
+//
+//
+// 示例 2:
+//
+//
+//输入: [3,2,3,1,2,4,5,5,6] 和 k = 4
+//输出: 4
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= k <= nums.length <= 10⁴
+// -10⁴ <= nums[i] <= 10⁴
+//
+// Related Topics 数组 分治 快速选择 排序 堆（优先队列） 👍 1277 👎 0
+package algs.sort;
+
+public class LC0215FindKthLargest {
+    public static void main(String[] args) {
+        int[] arr = {3, 2, 1, 5, 6, 4};
+        System.out.println(findKthLargest(arr, 2));
+    }
+
+    public static int findKthLargest(int[] nums, int k) {
+        int size = nums.length;
+        quickSort(nums, 0, size - 1);
+        return nums[size - k];
+    }
+
+    // quicksort the subarray from a[lo] to a[hi]
+    private static void quickSort(int[] a, int lo, int hi) {
+        if (hi <= lo) return;
+        int j = partition(a, lo, hi);
+        quickSort(a, lo, j - 1);
+        quickSort(a, j + 1, hi);
+    }
+
+    // partition the subarray a[lo..hi] so that a[lo..j-1] <= a[j] <= a[j+1..hi]
+    // and return the index j.
+    private static int partition(int[] a, int lo, int hi) {
+        int i = lo;
+        int j = hi + 1;
+        int v = a[lo];
+        while (true) {
+
+            // find item on lo to swap
+            while (less(a[++i], v)) {
+                if (i == hi) break;
+            }
+
+            // find item on hi to swap
+            while (less(v, a[--j])) {
+                if (j == lo) break;      // redundant since a[lo] acts as sentinel
+            }
+
+            // check if pointers cross
+            if (i >= j) break;
+
+            exch(a, i, j);
+        }
+
+        // put partitioning item v at a[j]
+        exch(a, lo, j);
+
+        // now, a[lo .. j-1] <= a[j] <= a[j+1 .. hi]
+        return j;
+    }
+
+    private static boolean less(int a, int b) {
+        return a < b;
+    }
+
+    private static void exch(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
