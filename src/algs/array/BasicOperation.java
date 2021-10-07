557680f4fcd58b3cec866879b75df9a701f7d8a3
a11 <syjefb2@outlook.com>
2021-10-07 17:46:08 +0800

    [algs][array] BasicOperation

--- src/algs/array/BasicOperation.java
+++ src/algs/array/BasicOperation.java
@@ -1,13 +1,21 @@
 package algs.array;
 
-public class BasicOperation {
-    public static void main(String[] args) {
-        int[] nums = {2,7,11,15};
+import java.util.Arrays;
 
-        // System.arraycopy();
-        // Arrays.copyOf();
-        // array to List, set
-        // array to hashmap
+public class BasicOperation {
+    private static final int[] integers = {2, 7, 11, 15};
+
+    public static void main(String[] args) {
+
+        // Arrays.copyOf
+        int[] copy1 = Arrays.copyOf(integers, integers.length - 1);
+        System.out.println(Arrays.toString(copy1));
+
+        // System.arraycopy
+        int[] copy2 = new int[integers.length - 1];
+        System.arraycopy(integers, 0, copy2, 0, integers.length - 1);
+        System.out.println(Arrays.toString(copy2));
+
     }
 
 }
