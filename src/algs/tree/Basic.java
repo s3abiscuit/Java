bc04c1e0ac1d91bc35bb68aa96e24f2832aeb37d
a11 <syjefb2@outlook.com>
2021-10-09 22:33:48 +0800

    [algs][tree] Add Basic

---
+++ src/algs/tree/Basic.java
@@ -0,0 +1,48 @@
+package algs.tree;
+
+public class Basic {
+    public static void main(String[] args) {
+
+    }
+
+    // 二叉树节点数量
+    public static int nodeCount(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + nodeCount(root.left) + nodeCount(root.right);
+    }
+
+    // 二叉树最大深度
+    public static int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+    public static TreeNode invertTree(TreeNode root) {
+        // base case
+        if (root == null) {
+            return null;
+        }
+
+        /**** 前序遍历位置 ****/
+        // root 节点需要交换它的左右子节点
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+
+        // 让左右子节点继续翻转它们的子节点
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
