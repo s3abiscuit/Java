e2844f24b9c0447fba68ff190a3a7cf3993c891a
a11 <syjefb2@outlook.com>
2021-10-10 21:21:58 +0800

    [algs][tree] Add LC0102LevelOrder

---
+++ src/algs/linkedlist/tree/levelorder/LC0102LevelOrder.java
@@ -0,0 +1,69 @@
+//给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+//
+//
+//
+// 示例：
+//二叉树：[3,9,20,null,null,15,7],
+//
+//
+//    3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+//
+//
+// 返回其层序遍历结果：
+//
+//
+//[
+//  [3],
+//  [9,20],
+//  [15,7]
+//]
+//
+// Related Topics 树 广度优先搜索 二叉树 👍 1045 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+package algs.linkedlist.tree.levelorder;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LC0102LevelOrder {
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root == null) return result;
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.remove();
+                level.add(node.val);
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            result.add(level);
+        }
+        return result;
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
