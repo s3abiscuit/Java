f9462e0f5fed2c2faf26181da0c768d084f57f9a
a11 <syjefb2@outlook.com>
2021-10-10 09:52:38 +0800

    [algs][tree] Update

---
+++ src/algs/tree/LC0114Flatten.java
@@ -0,0 +1,73 @@
+package algs.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LC0114Flatten {
+    public static void main(String[] args) {
+
+    }
+
+    // 定义：将以 root 为根的树拉平为链表
+    public void flatten(TreeNode root) {
+        // base case
+        if (root == null) return;
+
+        flatten(root.left);
+        flatten(root.right);
+
+        /**** 后序遍历位置 ****/
+        // 1、左右子树已经被拉平成一条链表
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+
+        // 2、将左子树作为右子树
+        root.left = null;
+        root.right = left;
+
+        // 3、将原先的右子树接到当前右子树的末端
+        TreeNode p = root;
+        while (p.right != null) {
+            p = p.right;
+        }
+        p.right = right;
+    }
+
+    public void flatten1(TreeNode root) {
+        List<TreeNode> list = new ArrayList<TreeNode>();
+        preorderTraversal(root, list);
+        int size = list.size();
+        for (int i = 1; i < size; i++) {
+            TreeNode prev = list.get(i - 1), curr = list.get(i);
+            prev.left = null;
+            prev.right = curr;
+        }
+    }
+
+    public void preorderTraversal(TreeNode root, List<TreeNode> list) {
+        if (root != null) {
+            list.add(root);
+            preorderTraversal(root.left, list);
+            preorderTraversal(root.right, list);
+        }
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ src/algs/tree/LC0226InvertTree.java
@@ -0,0 +1,65 @@
+//翻转一棵二叉树。
+//
+// 示例：
+//
+// 输入：
+//
+//      4
+//   /   \
+//  2     7
+// / \   / \
+//1   3 6   9
+//
+// 输出：
+//
+//      4
+//   /   \
+//  7     2
+// / \   / \
+//9   6 3   1
+//
+// 备注:
+//这个问题是受到 Max Howell 的 原问题 启发的 ：
+//
+// 谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 1020 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+package algs.tree;
+
+public class LC0226InvertTree {
+    public static void main(String[] args) {
+
+    }
+
+    // 先序遍历--从顶向下交换
+    static public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        // 保存右子树
+        TreeNode rightTree = root.right;
+        // 交换左右子树的位置
+        root.right = invertTree(root.left);
+        root.left = invertTree(rightTree);
+        return root;
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
